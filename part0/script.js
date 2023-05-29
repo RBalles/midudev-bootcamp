b9ab1d5c9e02e97652a75c7884f48a5b2cfb447a
RBalles <ramonballesteros1@gmail.com>
2023-05-29 18:38:38 +0200

    Primer commit

---
+++ part0/script.js
@@ -0,0 +1,13 @@
+const list = [];
+const anotherList = list.concat(157)
+
+const persona = {
+    name: 'RB',
+    instagram: '@instagramRB',
+    age: 18,
+    isDeveloper: true,
+    links: ['https:\\ramonballesteros.com']
+}
+
+console.log(persona.name)
+console.log(persona.links)
